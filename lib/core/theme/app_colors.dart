bcde997aba8d7bce6854d37faf712ae57d9d1ac5
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-16 22:09:03 +0200

    Create AppColors file

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // ===== Primary =====
+  static const Color primaryBackground = Color(0xFF171827);
+  static const Color primaryAccent = Color(0xFFD2B5FF);
+
+  // ===== Secondary =====
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGrey = Color(0xFFDADADA);
+  static const Color grey = Color(0xFF797979);
+}
