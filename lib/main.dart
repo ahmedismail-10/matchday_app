4f3364da62de85c7eebcf5ce77927903ef497938
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-16 22:26:58 +0200

    Change theme & Create HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matchday/core/theme/app_colors.dart';
 
 void main() {
   runApp(const MatchDayApp());
@@ -11,6 +12,9 @@ class MatchDayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.primaryBackground,
+      ),
       home: Scaffold(),
     );
   }
